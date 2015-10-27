7752767ef97cf772c02885906d5e693e2109972d
Nilton Leme Pereira <amorim@muranodesign.com.br>
2015-10-27 12:32:40 -0200

    Retirando o # da url

--- angular/app/app.js
+++ angular/app/app.js
@@ -1,9 +1,13 @@
 var app = angular.module('app',['ngRoute']);
 
 //creating route
-app.config(['$routeProvider',function($routeProvider){
+app.config(['$routeProvider','$locationProvider',function($routeProvider,$locationProvider){	
+
+	$locationProvider.html5Mode(true);
+
 	$routeProvider.
 	when('/',{controller:'loginCTRL', templateUrl:'index.html'});
 }]);
+
 
 
