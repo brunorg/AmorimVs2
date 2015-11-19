094c98fa129b6501c98a82f5d230eddd3aa494f2
Nilton Leme Pereira <amorim@muranodesign.com.br>
2015-11-19 15:11:10 -0200

    Alterações Diversas

--- js/alterarNome.js
+++ js/alterarNome.js
@@ -6,31 +6,47 @@ $(document).ready(function () {
 		async:false,
 		crossDomain: true,
 		success: function(data)
-		{		
+		{								
 			for(var i=0;i<data.length;i++){
-				var nome1="";	
-				var nome = data[i].nome;
-				nome = nome.split(" ");	
-				
-				var sobreNP = ["do", "da", "das", "e", "dos", "de"];
-				var bool = false;
-								
-				for(var j=0;j<nome.length;j++){
-					for(var k=0;k<sobreNP.length;k++){
-						if(sobreNP[k] == nome[j].toLowerCase()){
-							bool = true;
+				//console.log(data.length,'----',data[i].idAluno);				
+				if(data[i].idAluno>558){			
+					var nome1="";	
+					var nome = data[i].nome;					
+					nome = nome.trim();
+					console.log(nome);
+					nome = nome.split(" ");	
+					
+					var sobreNP = ["do", "da", "das", "e", "dos", "de"];
+					var bool = false;
+									
+					for(var j=0;j<nome.length;j++){
+						for(var k=0;k<sobreNP.length;k++){
+							if(sobreNP[k] == nome[j].toLowerCase()){
+								bool = true;
+							}
 						}
+						
+						if(!bool){
+							nome1 += nome[j][0].toUpperCase()+nome[j].substr(1,nome[j].length).toLowerCase()+" ";					
+						}else{
+							nome1 += nome[j].toLowerCase()+" ";
+							bool = false;
+						}
+						//nome1 += nome[j][0].toUpperCase()+nome[j].substr(1,nome[j].length).toLowerCase()+" ";				
 					}
 					
-					if(!bool){
-						nome1 += nome[j][0].toUpperCase()+nome[j].substr(1,nome[j].length).toLowerCase()+" ";					
-					}else{
-						nome1 += nome[j].toLowerCase()+" ";
-						bool = false;
-					}
-					//nome1 += nome[j][0].toUpperCase()+nome[j].substr(1,nome[j].length).toLowerCase()+" ";				
+					$.ajax({
+						url: path+"Alunos/AlunoUpdate",
+						type: "POST",
+						async:false,
+						crossDomain: true,
+						data: "nome="+nome1.trim()+"&id="+data[i].idAluno,
+						success: function(data)
+						{			
+							console.log('alterou');	
+						}
+					});					
 				}
-				//console.log(nome1.trim());
 				//Alterar nome do professor
 				/*$.ajax({
 					url: path+"ProfessorFuncionario/ProfessorUpdate",
@@ -40,21 +56,10 @@ $(document).ready(function () {
 					data: "nome="+nome1.trim()+"&id="+data[i].idprofessorFuncionario,
 					success: function(data)
 					{			
-						console.log('alterou');	
+						console.log(data[i].nome,'-----',nome1.trim());	
 					}
-				});
+				});*/			
 				
-				$.ajax({
-					url: path+"Alunos/AlunoUpdate",
-					type: "POST",
-					async:false,
-					crossDomain: true,
-					data: "nome="+nome1.trim()+"&id="+data[i].idAluno,
-					success: function(data)
-					{			
-						console.log('alterou');	
-					}
-				});*/
 			}			
 		}
 	});
