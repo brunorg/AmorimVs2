0a940f0bb67ee9679a73342bc20e7b95b4655d2a
Nilton Leme Pereira <amorim@muranodesign.com.br>
2016-01-06 17:22:51 -0200

    ano letivo fix, pagina tutoria ok

--- js/anoLetivo.js
+++ js/anoLetivo.js
@@ -1,21 +1,21 @@
 // JavaScript Document
 function diasLetivos(){
-	
+
 	var anoLetivo = new Date();
-	anoLetivo = anoLetivo.getFullYear();		
+	anoLetivo = anoLetivo.getFullYear();
 	var countUteis = 0;
-	
+
 	var dataInicial = anoLetivo + "-01-01";
 	var dataFinal =  anoLetivo + "-12-31";
 	var ano;
 	var feriadosDoAno = new Array();
 	var cont = 0;
-	
-	
+
+
 	var dataCalendario = getData("Calendario/feriados/"+anoLetivo, null);
-	
+
 	//console.log(dataCalendario);
-	
+
 	$.ajax({
 		url: path+"Calendario/feriados/"+anoLetivo,
 		type: "GET",
@@ -23,16 +23,16 @@ function diasLetivos(){
 		dataType: 'json',
 		async:false,
 		success: function(dataCalendario){
-			for(var i=0;i<dataCalendario.length;i++) {				
-				var data="";		
-				if((dataCalendario[i].dataInicio != null)&&(dataCalendario[i].dataFim != null)){		
-					var dias = diasDecorridos(dataCalendario[i].dataInicio, dataCalendario[i].dataFim);			
-					var diaIn = dataCalendario[i].dataInicio;			
+			for(var i=0;i<dataCalendario.length;i++) {
+				var data="";
+				if((dataCalendario[i].dataInicio != null)&&(dataCalendario[i].dataFim != null)){
+					var dias = diasDecorridos(dataCalendario[i].dataInicio, dataCalendario[i].dataFim);
+					var diaIn = dataCalendario[i].dataInicio;
 					diaIn = diaIn.split("-");
-							
+
 					var dataInicial =(diaIn[0]+"-"+diaIn[1]+"-"+diaIn[2]);
-					for(var j=0;j<=dias;j++){				
-						if(dataCalendario[i].dataInicio == dataInicial){					
+					for(var j=0;j<=dias;j++){
+						if(dataCalendario[i].dataInicio == dataInicial){
 							data = dataCalendario[i].dataInicio;
 							data = data.split("-");
 							data = data[2]+"-"+data[1]+"-"+data[0];
@@ -42,8 +42,8 @@ function diasLetivos(){
 						}else{
 							feriadosDoAno[cont] = (((parseInt(diaIn[2])+j).toString().length< 2) ? "0"+(parseInt(diaIn[2])+j) : (parseInt(diaIn[2])+j))+"-"+diaIn[1]+"-"+diaIn[0];
 							cont++;
-						}													
-					}							
+						}
+					}
 				}else if((dataCalendario[i].dataInicio != null)&&(dataCalendario[i].dataFim == null)){
 					if(dataCalendario[i].dataInicio != null){
 						data = dataCalendario[i].dataInicio;
@@ -52,51 +52,52 @@ function diasLetivos(){
 						feriadosDoAno[cont] = data;
 						cont++;
 					}
-				}			
+				}
 			}
-			
+
 			dataInicial = Date.parse(dataInicial);
 			dataFinal = Date.parse(dataFinal)-1;
-				
+
 			while (dataInicial <= dataFinal) {
 				myDate = new Date(dataInicial);
-				myDate.setDate(myDate.getDate()+1);	
-				
+				myDate.setDate(myDate.getDate()+1);
+
 				dataInicial = myDate;
-				
-				dia = dataInicial.getDay();	
-				
-				var diaAno = myDate.toLocaleDateString();	
-			
+
+				dia = dataInicial.getDay();
+
+				var diaAno = myDate.toLocaleDateString();
+
 				if(dia!="0" && dia != "6"){
-					
-					countUteis++;	
-							
-					$.each(feriadosDoAno, function( key, value ) {	
-						var feriado = value.split('-');			
-						feriado = parseInt(feriado[0])+"/"+parseInt(feriado[1])+"/"+feriado[2];	
+
+					countUteis++;
+
+					$.each(feriadosDoAno, function( key, value ) {
+						var feriado = value.split('-');
+						feriado = parseInt(feriado[0])+"/"+parseInt(feriado[1])+"/"+feriado[2];
 						diaAno  = diaAno.split("/");
-						diaAno = parseInt(diaAno[0])+"/"+parseInt(diaAno[1])+"/"+diaAno[2];				
+						diaAno = parseInt(diaAno[0])+"/"+parseInt(diaAno[1])+"/"+diaAno[2];
 						if(feriado == diaAno){
 							countUteis--;
-						}				
-					})			
-				}				
+						}
+					})
+				}
 			}
-			return countUteis;		
 		}
-	});	
+	});
+
+	return countUteis;
 }
 
 function diasDecorridos(dt1, dt2){
     // variáveis auxiliares
-    var minuto = 60000; 
+    var minuto = 60000;
     var dia = minuto * 60 * 24;
     var horarioVerao = 0;
-    
+
 	var dt1 = new Date(dt1);
 	var dt2 = new Date(dt2);
-	
+
     // ajusta o horario de cada objeto Date
     dt1.setHours(0);
     dt1.setMinutes(0);
@@ -104,19 +105,19 @@ function diasDecorridos(dt1, dt2){
     dt2.setHours(0);
     dt2.setMinutes(0);
     dt2.setSeconds(0);
-    
+
     // determina o fuso horário de cada objeto Date
     var fh1 = dt1.getTimezoneOffset();
-    var fh2 = dt2.getTimezoneOffset(); 
-    
+    var fh2 = dt2.getTimezoneOffset();
+
     // retira a diferença do horário de verão
     if(dt2 > dt1){
       horarioVerao = (fh2 - fh1) * minuto;
-    } 
-    else{
-      horarioVerao = (fh1 - fh2) * minuto;    
     }
-    
+    else{
+      horarioVerao = (fh1 - fh2) * minuto;
+    }
+
     var dif = Math.abs(dt2.getTime() - dt1.getTime()) - horarioVerao;
     return Math.ceil(dif / dia);
 }
--- js/grupoTutoria.js
+++ js/grupoTutoria.js
@@ -1,30 +1,70 @@
 /*global $:false */
 'use strict';
+var professorId= localStorage.getItem("professorId");
 
-function criarPainelDoGrupo(idDivPrincipal, idDivGrupo, dados) {
+function criarPainelDoGrupo(idDivPrincipal, idDivGrupo, retornoAjax) {
 
 	$('#' + idDivPrincipal).append('<div class="grupoBox" id="' + idDivGrupo + '"></div>');
+
 	$('#' + idDivGrupo).append('<div class="grupoBlocoNomes"></div>')
-	$('#' + idDivGrupo + " .grupoBlocoNomes").append('<div class="grupoNome">' + 'Grupo 6M1' + '</div>') //Nome do grupo
+	$('#' + idDivGrupo + " .grupoBlocoNomes").append('<div class="grupoNome">' + retornoAjax.grupoNome + '</div>') //Nome do grupo
 	$('#' + idDivGrupo + " .grupoBlocoNomes").append('<div class="caixaGL"></div>')
-	$('#' + idDivGrupo + " .grupoBlocoNomes").append('<div class="grupoBlocoGraficoObj"></div>')
-	$('#' + idDivGrupo + " .grupoBlocoNomes .grupoBlocoGraficoObj").append('<div class="grupoGraficoTitulo grupoTituloBottom">OBJETIVOS</div>')
 
-	//criarPaineisDosAlunos(idDivGrupo) ;
+	$('#' + idDivGrupo).append('<div class="grupoBlocoGraficoObj"></div>')
+	$('#' + idDivGrupo + " .grupoBlocoGraficoObj").append('<div class="grupoGraficoTitulo grupoTituloBottom">OBJETIVOS</div>')
+	$('#' + idDivGrupo + " .grupoBlocoGraficoObj").append('<table class="grupoTabela"></table>')
+	$('#' + idDivGrupo + " .grupoBlocoGraficoObj .grupoTabela").append('<thead></thead>')
+	$('#' + idDivGrupo + " .grupoBlocoGraficoObj .grupoTabela").append('<tbody></tbody>')
+	$('#' + idDivGrupo + " .grupoBlocoGraficoObj .grupoTabela thead").append('<th>5</th><th>10</th><th>15</th><th>20</th><th>25</th><th>30</th><th>35</th><th>40</th><th>45</th><th>50</th><th>55</th><th>60</th><th>65</th><th>70</th><th>75</th><th>80</th><th>85</th><th>90</th><th>95</th><th>100</th><th>%</th>')
+	$('#' + idDivGrupo).append('<div class="grupoBlocoGraficoFreq"></div>')
+
+	$('#' + idDivGrupo + " .grupoBlocoGraficoFreq").append('<div class="grupoGraficoTituloLeft">FREQUÊNCIA</div>')
+	$('#' + idDivGrupo + " .grupoBlocoGraficoFreq").append('<table class="grupoTabela grupoTabelaFreq"></table>')
+	$('#' + idDivGrupo + " .grupoBlocoGraficoFreq").append('<div class="caixaGR"></div>')
+	$('#' + idDivGrupo + " .grupoBlocoGraficoFreq .grupoTabela").append('<thead></thead>')
+	$('#' + idDivGrupo + " .grupoBlocoGraficoFreq .grupoTabela").append('<tbody></tbody>')
+	$('#' + idDivGrupo + " .grupoBlocoGraficoFreq .grupoTabela thead").append('<th>10</th><th>20</th><th>30</th><th>40</th><th>50</th><th>60</th><th>70</th><th>80</th><th>90</th><th>100</th><th>%</th>')
+
+	criarPaineisDosAlunos(idDivGrupo, retornoAjax.grupoAlunos) ;
 }
 
-function criarPaineisDosAlunos(idDivGrupo) {
-	for (var i = 5; i > 0; i--) {
+function criarPaineisDosAlunos(idDivGrupo, grupoAlunos) {
 
-		criarBarrinhas(idDivGrupo + 'aluno' + i, dadosAluno);
-	}
+	for (var i = grupoAlunos.length - 1; i >= 0; i--) {
+
+		$('#' + idDivGrupo + " .grupoBlocoNomes .caixaGL").append('<div class="grupoLinhaAluno borderLeft" id="aluno' + i + '"></div>')
+
+		$('#' + idDivGrupo + " .grupoBlocoNomes .caixaGL .grupoLinhaAluno#aluno" + i).append('<div class="grupoFotoAluno"></div>')
+		$('#' + idDivGrupo + " .grupoBlocoNomes .caixaGL .grupoLinhaAluno#aluno" + i + " .grupoFotoAluno").append('<img src="' + grupoAlunos[i].foto + '"></img>')
+
+		$('#' + idDivGrupo + " .grupoBlocoNomes .caixaGL .grupoLinhaAluno#aluno" + i).append('<div class="grupoNomeAluno"></div>')
+		$('#' + idDivGrupo + " .grupoBlocoNomes .caixaGL .grupoLinhaAluno#aluno" + i + " .grupoNomeAluno").append('<span>' + grupoAlunos[i].nome + '</span>')
+
+		$('#' + idDivGrupo + " .grupoBlocoNomes .caixaGL .grupoLinhaAluno#aluno" + i).append('<div class="grupoCurtir"></div>')
+		$('#' + idDivGrupo + " .grupoBlocoNomes .caixaGL .grupoLinhaAluno#aluno" + i + " .grupoCurtir").append('<p class="grupo">&nbsp;</p>')
+
+		$('#' + idDivGrupo + " .grupoBlocoGraficoObj .grupoTabela tbody").append('<tr id="aluno' + i + '"></tr>')
+		$('#' + idDivGrupo + " .grupoBlocoGraficoObj .grupoTabela tbody tr#aluno" + i).append('<td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td class="ultimaColunaBBranca"></td>')
+		if (i != 0) {
+			$('#' + idDivGrupo + " .grupoBlocoGraficoObj .grupoTabela tbody").append('<tr class="tableSeparacao"><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td></tr>')
+		}
+
+		$('#' + idDivGrupo + " .grupoBlocoGraficoFreq .grupoTabela tbody").append('<tr id="aluno' + i + 'freq"></tr>')
+		$('#' + idDivGrupo + " .grupoBlocoGraficoFreq .grupoTabela tbody tr#aluno" + i + "freq").append('<td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td class="ultimaColunaBBranca"></td>')
+		if (i != 0) {
+			$('#' + idDivGrupo + " .grupoBlocoGraficoFreq .grupoTabela tbody").append('<tr class="tableSeparacao"><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td></tr>')
+		}
+		$('#' + idDivGrupo + " .grupoBlocoGraficoFreq .caixaGR").append('<div class="grupoLinhaAluno grupoLinhaAlunoTop borderRight"><div class="grupoGraficoFreqTitulo"><h1 class="freqTitulo">Presenças</h1><h2 class="anoLetivo" id="anoLetivo">' + grupoAlunos[i].presencas +'</h2><h1 class="freqTitulo">Faltas</h1><h2 class="faltas" id="faltas">' + grupoAlunos[i].faltas + '</h2></div></div>')
+
+		criarBarrinhas(idDivGrupo, 'aluno' + i, grupoAlunos[i]);
+	};
 
 }
 
-function adicionarBarrinhasNoHtml(id) {
+function adicionarBarrinhasNoHtml(idDivGrupo, id) {
 
 	// Adiciona as divs de barrinha de progressão do aluno
-	$('#' + id + ' td:first-child').html('<div class="barrinhaPorcentagem atual"></div>'+
+	$('#' + idDivGrupo + " .grupoBlocoGraficoObj .grupoTabela tbody" + ' #' + id + ' td:first-child').html('<div class="barrinhaPorcentagem atual"></div>'+
 	                                     '<div class="barrinhaPorcentagem atualCorrigido"></div>'+
 	                                     '<div class="barrinhaPorcentagem anterior"></div>'+
 	                                     '<div class="barrinhaPorcentagem anteriorCorrigido"></div>'+
@@ -32,69 +72,70 @@ function adicionarBarrinhasNoHtml(id) {
 	                                     '<div class="barrinhaPorcentagem proximaCorrigido"></div>');
 
 	// Adiciona as divs de barrinha de faltas do mesmo aluno
-	$('#' + id + 'freq' + ' td:first-child').html('<div class="barrinhaPorcentagem diasLetivos"></div>'+
+	$('#' + idDivGrupo + " .grupoBlocoGraficoFreq .grupoTabela tbody" + ' #' + id + 'freq' + ' td:first-child').html('<div class="barrinhaPorcentagem diasLetivos"></div>'+
                                     			  '<div class="barrinhaPorcentagem diasLetivosFaltas"></div>');
 }
 
-function criarBarrinhas(id, dados) {
+function criarBarrinhas(idDivGrupo, id, dadosAluno) {
 
 	//Adiciona as divs das barrinhas, todas tem 0 de largura por padrão
-	adicionarBarrinhasNoHtml(id);
+	adicionarBarrinhasNoHtml(idDivGrupo, id);
 
 	// Pega o tamanho da div que irá conter as barrinhas e define tamanho fixo de um ponto percentual dentro dela
-	var primeiraTabela = document.getElementById(id);
-	var primeiraTabelaRect = primeiraTabela.getBoundingClientRect();
-	var primeiraTabelaLargura = primeiraTabelaRect.width - 18;
-	var tamanhoPontoPercentual = primeiraTabelaLargura / 100;
 
-	var segundaTabela = document.getElementById(id + 'freq');
-	var segundaTabelaRect = segundaTabela.getBoundingClientRect();
-	var segundaTabelaLargura = segundaTabelaRect.width - 18;
-	var tamanhoPontoPercentual2 = segundaTabelaLargura / 100;
+	var tamanhoPontoPercentual = 317 / 100;
 
-	// Define tamanho de cada barrinha de acordo com os dados
-	$('#' + id + 		  ' .atual'				).css('width', dados.atual 				* tamanhoPontoPercentual);
-	$('#' + id + 		  ' .atualCorrigido'	).css('width', dados.atualCorrigido 	* tamanhoPontoPercentual);
-	$('#' + id + 		  ' .anterior'			).css('width', dados.anterior 			* tamanhoPontoPercentual);
-	$('#' + id + 		  ' .anteriorCorrigido'	).css('width', dados.anteriorCorrigido 	* tamanhoPontoPercentual);
-	$('#' + id + 		  ' .proxima'			).css('width', dados.proxima 			* tamanhoPontoPercentual);
-	$('#' + id + 		  ' .proximaCorrigido'	).css('width', dados.proximaCorrigido 	* tamanhoPontoPercentual);
-	$('#' + id + 'freq' + ' .diasLetivos'		).css('width', dados.diasLetivos 		* tamanhoPontoPercentual2);
-	$('#' + id + 'freq' + ' .diasLetivosFaltas'	).css('width', dados.diasLetivosFaltas 	* tamanhoPontoPercentual2);
+	var tamanhoPontoPercentual2 = 157 / 100;
+
+	var dadosAluno2 = {
+		'atual' 			: dadosAluno.objetivosAnoAtual.completos * 100,
+		'atualCorrigido' 	: dadosAluno.objetivosAnoAtual.corrigidos * 100,
+		'anterior'			: dadosAluno.objetivosPendentes.completos * 100,
+		'anteriorCorrigido' : dadosAluno.objetivosPendentes.completos * 100,
+		'proxima'			: dadosAluno.objetivosFuturo.completos * 100,
+		'proximaCorrigido'	: dadosAluno.objetivosFuturo.completos * 100,
+		'diasLetivos'		: dadosAluno.presencas/totalDiasLetivos * 100,
+		'diasLetivosFaltas'	: dadosAluno.faltas/totalDiasLetivos * 100,
+	};
+
+	console.log(dadosAluno2.diasLetivos + " / " + dadosAluno2.diasLetivosFaltas)
+
+	// Define tamanho de cada barrinha de acordo com os dadosAluno
+	$('#' + idDivGrupo + " .grupoBlocoGraficoObj .grupoTabela tbody" + ' #' + id + 		  ' .atual'				).css('width', dadosAluno2.atual 				* tamanhoPontoPercentual);
+	$('#' + idDivGrupo + " .grupoBlocoGraficoObj .grupoTabela tbody" + ' #' + id + 		  ' .atualCorrigido'	).css('width', dadosAluno2.atualCorrigido 	* tamanhoPontoPercentual);
+	$('#' + idDivGrupo + " .grupoBlocoGraficoObj .grupoTabela tbody" + ' #' + id + 		  ' .anterior'			).css('width', dadosAluno2.anterior 			* tamanhoPontoPercentual);
+	$('#' + idDivGrupo + " .grupoBlocoGraficoObj .grupoTabela tbody" + ' #' + id + 		  ' .anteriorCorrigido'	).css('width', dadosAluno2.anteriorCorrigido 	* tamanhoPontoPercentual);
+	$('#' + idDivGrupo + " .grupoBlocoGraficoObj .grupoTabela tbody" + ' #' + id + 		  ' .proxima'			).css('width', dadosAluno2.proxima 			* tamanhoPontoPercentual);
+	$('#' + idDivGrupo + " .grupoBlocoGraficoObj .grupoTabela tbody" + ' #' + id + 		  ' .proximaCorrigido'	).css('width', dadosAluno2.proximaCorrigido 	* tamanhoPontoPercentual);
+	$('#' + idDivGrupo + " .grupoBlocoGraficoFreq .grupoTabela tbody" + ' #' + id + 'freq' + ' .diasLetivos'		).css('width', dadosAluno2.diasLetivos 		* tamanhoPontoPercentual2);
+	$('#' + idDivGrupo + " .grupoBlocoGraficoFreq .grupoTabela tbody" + ' #' + id + 'freq' + ' .diasLetivosFaltas'	).css('width', dadosAluno2.diasLetivosFaltas 	* tamanhoPontoPercentual2);
 }
 
 // vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv Debug vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv
 
-var aluno1dados = {
-	'atual' 			: 75,
-	'atualCorrigido' 	: 25,
-	'anterior'			: 100,
-	'anteriorCorrigido' : 100,
-	'proxima'			: 10,
-	'proximaCorrigido'	: 5,
-	'diasLetivos'		: 100,
-	'diasLetivosFaltas'	: 10
-};
-
-var aluno2dados = {
-	'atual' 			: 50,
-	'atualCorrigido' 	: 10,
-	'anterior'			: 0,
-	'anteriorCorrigido' : 0,
-	'proxima'			: 0,
-	'proximaCorrigido'	: 0,
-	'diasLetivos'		: 25,
-	'diasLetivosFaltas'	: 10
-};
-
-var dados = {
-	"grupo1"
-}
-
+var totalDiasLetivos = diasLetivos();
+console.log(diasLetivos())
 
 window.onload = function() {
 
-	criarPainelDoGrupo('divPrincipalGrupos', 'grupo1', dados);
+	$.ajax({
+        url: path + "Grupo/TutoriaDados/55",
+        async: false,
+        crossDomain: true,
+        type: "GET",
+        success: function(retornoAjax){
+
+            for (var i = 0; i < retornoAjax.length ; i++) {
+
+                criarPainelDoGrupo('divPrincipalGrupos', 'grupo' + i, retornoAjax[i]);
+
+            };
+
+        },
+        error: function(a, status, error) {
+            console.log(status + " /// " + error)
+        }
+    });
 
 	//criarBarrinhas('aluno1', aluno1dados);
 	//criarBarrinhas('aluno2', aluno2dados);
