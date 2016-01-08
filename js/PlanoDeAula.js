6c4b209b756ab53f52efd76549008b6d40a6d45f
Nilton Leme Pereira <amorim@muranodesign.com.br>
2016-01-08 12:21:43 -0200

    plano de aula

--- js/PlanoDeAula.js
+++ js/PlanoDeAula.js
@@ -179,6 +179,10 @@ $(document).ready(function() {
 		showOn: "button",
 		beforeShow: function () {
 			$("#box_novo").css("height", "285px");
+			setTimeout(recolorCalendar, 1);
+		},
+		onChangeMonthYear: function() {
+			setTimeout(recolorCalendar, 1);
 		},
 		onClose: function () {
 			$("#box_novo").css("height", "130px");
@@ -201,11 +205,15 @@ $(document).ready(function() {
 		monthNames: ['JANEIRO','FEVEREIRO','MARÇO','ABRIL','MAIO','JUNHO','JULHO','AGOSTO',
 					 'SETEMBRO','OUTUBRO','NOVEMBRO','DEZEMBRO']	
 	}).next(".ui-datepicker-trigger").css('background-color',corMedia);
-	
+
 	$( "#data_fim" ).datepicker({
 		showOn: "button",
 		beforeShow: function () {
 			$("#box_novo").css("height", "285px");
+			setTimeout(recolorCalendar, 1);
+		},
+		onChangeMonthYear: function() {
+			setTimeout(recolorCalendar, 1);
 		},
 		onClose: function () {
 			if(!($("#data_inicio").val() != "" && $("#data_fim").val() != ""))
@@ -227,6 +235,23 @@ $(document).ready(function() {
 		monthNames: ['JANEIRO','FEVEREIRO','MARÇO','ABRIL','MAIO','JUNHO','JULHO','AGOSTO',
 					 'SETEMBRO','OUTUBRO','NOVEMBRO','DEZEMBRO']	
 	}).next(".ui-datepicker-trigger").css('background-color',corMedia);
+
+
+
+
+
+
+	function recolorCalendar(){
+		$(".ui-datepicker-title").css("background-color", corFraca);
+		$(".ui-datepicker th").css("background-color", corMedia);
+		//$(".ui-corner-all").css("background-color", corForte);
+		console.log("ay")
+	}
+
+
+
+
+
 	
 	$(".Conteudo_Coluna").scroll(function(){
 		$("#box_novo").removeClass("exibir");
