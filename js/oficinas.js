ca3c1ae3b2b1e0184c78416baf4c00058588d35d
Lucas Tavares <amorim@muranodesign.com.br>
2016-01-27 15:30:57 -0200

    oficinas

--- js/oficinas.js
+++ js/oficinas.js
@@ -1,6 +1,6 @@
-var listaOficinasAluno = [];
-
 $(document).ready(function(){
+	$(".scroll_receiver").mCustomScrollbar({ axis: "y" });
+	
 	requestOficinasAluno();
 });
 
@@ -49,6 +49,34 @@ function getRoteirosPorOficina(idoficina) {
 
 	return retorno;
 }
+function getObjetivosPorRoteiro(idroteiro) {
+	var retorno;
+
+	$.ajax({
+		url: path + 'ObjetivoAula/ListarPorRoteiro/' + idroteiro,
+		async: false,
+		type: "GET",
+		crossDomain: true,
+		beforeSend: function() { loading("inicial"); },
+		success: function(dataObj) { retorno = dataObj },
+		complete: function() { loading("final"); }
+	});
+
+	return retorno;
+}
+function getImagemPorPostagem(idpostagem) {
+	var retorno;
+
+	$.ajax({
+		url: path + "Blog/ImagemMed/" + idpostagem,
+		type: "GET",
+		async: false,
+		crossDomain: true,
+		success: function(dImagem) { retorno = dImagem; }
+	});
+
+	return retorno;
+}
 
 function requestOficinasAluno() {
 	var alunoVarObj = JSON.parse(localStorage.objetoAlunoVariavel);
@@ -62,13 +90,10 @@ function requestOficinasAluno() {
 
 
 		for ( i in oficinas ) {
-			listaOficinasAluno[i] = oficinas[i];
-			console.info("Nome: "+oficinas[i].nome+", id: "+oficinas[i].idoficina);
-
 			htmlOficinas += "<td>";
+			htmlOficinas += 	"<div class=\"fundo_aba\" style=\"background:"+oficinas[i].tipoOficina.cor.forte+"\">&nbsp;</div>";
 			htmlOficinas += 	"<div id=\"oficina"+oficinas[i].idoficina+"\" class=\"aba_oficina\" onclick=\"accessOficina("+oficinas[i].idoficina+", \'"+oficinas[i].tipoOficina.cor.forte+"\', \'"+oficinas[i].tipoOficina.cor.medio+"\', \'"+oficinas[i].tipoOficina.cor.fraco+"\')\">";
-			htmlOficinas +=			"<p class=\"barra_cor_of\" style=\"background:"+oficinas[i].tipoOficina.cor.forte+"\">&nbsp;</p>";
-			htmlOficinas +=			"<p class=\"titulo_of\">"+oficinas[i].tipoOficina.nome+"</p>";
+			htmlOficinas +=			"<p class=\"oficina_titulo\">"+oficinas[i].tipoOficina.nome+"</p>";
 			htmlOficinas +=		"</div>";
 			htmlOficinas +=	"</td>";
 		}
@@ -86,7 +111,8 @@ function requestBlogPostagensPorOficina(idOficina) {
 	var htmlBlog = new String();
 
 	if ( blog.length > 0 ) {
-		htmlBlog += "<div class=\"Postagens_Container\">";
+		htmlBlog += "<div class=\"blog_postagens scroll_limiter\">";
+		htmlBlog += 	"<div class=\"postagens_container\">";
 
 		for ( var i = blog.length-1; i >= 0; i-- ) {
 			var dia 	 = blog[i].data.slice(8);
@@ -98,18 +124,28 @@ function requestBlogPostagensPorOficina(idOficina) {
 	        htmlBlog +=	   	"<h1 class=\"cx_titulo\">"+blog[i].titulo+"</h1>";
 	        htmlBlog +=	   	"<h2 class=\"cx_info\">"+dia+'/'+mes+'/'+ano+"</h2>";
 
-	        if ( blog[i].imagem ) { htmlBlog += "<img src=\""+blog[i].imagem+"\" class=\"img_postagem\" />" };
-    	    for ( j in conteudo ) { htmlBlog += post[j] ? "<p class=\"cx_texto\">"+post[j]+"</p>": ""; }
+	        if ( blog[i].imagem ) {
+	        	var imagem = requestImagemPorPostagem(blog[i].idblog);
+
+	        	htmlBlog += imagem;
+	        	console.log(imagem);
+	        }
+
+    	    for ( j in conteudo ) { htmlBlog += conteudo[j] ? "<p class=\"cx_texto\">"+conteudo[j]+"</p>": ""; }
 
 			htmlBlog += 	"<hr class=\"fim_postagem\" />";
         	htmlBlog +=	"</article>";
 		}
+
+		htmlBlog += 	"</div>";
 		htmlBlog += "</div>";
 	} else {
-		htmlBlog +=	"<div class=\"Postagens_Container\">";
+		htmlBlog +=	"<div class=\"blog_postagens\">";
 		htmlBlog +=		"<p class=\"feedback_oficinas_false\">Ainda não foram realizadas postagens para esta oficina.</p>";
 		htmlBlog += "</div>";
 	}
+
+	return htmlBlog;
 }
 function requestRoteirosPorOficina(idoficina, corForte, corMedia, corFraca) {
 	var roteiros 	 = getRoteirosPorOficina(idoficina);
@@ -118,17 +154,17 @@ function requestRoteirosPorOficina(idoficina, corForte, corMedia, corFraca) {
 	if (roteiros.length > 0) {
 		var roteiroNumero = 1;
 		for ( var a in roteiros ) {
-			htmlRoteiros += "<div id=\"roteiro_"+roteiros[a].idroteiro_aula+"\" class=\"oficina_plan_linha\">";
-			htmlRoteiros += 	"<div class=\"oficina_planejamento_info\" style=\"background-color: "+corFraca+";\" onclick=\"acordeon("+roteiros[a].idroteiro_aula+")\">";
-    		htmlRoteiros +=			"<div class=\"Oficina_Plan_Num\" style=\"background-color: "+corForte+";\">"+roteiroNumero+"</div>";
-    		htmlRoteiros +=			"<div class=\"Oficina_Plan_Nome\">"+roteiros[a].roteiro+"</div>";
+			htmlRoteiros += "<div id=\"roteiro_"+roteiros[a].idroteiro_aula+"\" class=\"oficina_planejamento\">";
+			htmlRoteiros += 	"<div class=\"roteiro_info\" style=\"background-color: "+corFraca+";\" onclick=\"toggleRoteiro("+roteiros[a].idroteiro_aula+")\">";
+    		htmlRoteiros +=			"<div class=\"roteiro_num_listagem\" style=\"background-color: "+corForte+";\">"+roteiroNumero+"</div>";
+    		htmlRoteiros +=			"<div class=\"roteiro_nome\">"+roteiros[a].roteiro+"</div>";
     		htmlRoteiros +=		"</div>";
-    		htmlRoteiros +=		"<div class=\"Oficina_Plan_Itens\">";
-    		htmlRoteiros +=			"<div class=\"Oficina_Plan_Desc Oficina_Plan_Item\">Descrição: "+roteiros[a].descricao+"</div>";
-			htmlRoteiros +=			"<div class=\"Oficina_Plan_Recursos Oficina_Plan_Item\">";
-			htmlRoteiros +=				"<div class=\"Oficina_Recurso Rec_Livro\">Lorem ipsum dolor sit amet</div>";
-			htmlRoteiros +=				"<div class=\"Oficina_Recurso Rec_Video\">Consectetur adipiscing elit</div>";
-			htmlRoteiros +=			"</div>";
+    		htmlRoteiros +=		"<div class=\"roteiro_conteudo\">";
+    		htmlRoteiros +=			"<div class=\"roteiro_descricao roteiro_item\">Descrição: "+roteiros[a].descricao+"</div>";
+			//htmlRoteiros +=			"<div class=\"roteiro_recusos roteiro_item\">";
+			//htmlRoteiros +=				"<div class=\"roteiro_recuso recurso_livro\">Lorem ipsum dolor sit amet</div>";
+			//htmlRoteiros +=				"<div class=\"roteiro_recuso recurso_video\">Consectetur adipiscing elit</div>";
+			//htmlRoteiros +=			"</div>";
     		htmlRoteiros +=		"</div>";
     		htmlRoteiros +=	"</div>";
 
@@ -139,6 +175,39 @@ function requestRoteirosPorOficina(idoficina, corForte, corMedia, corFraca) {
 	}
 	return htmlRoteiros;
 }
+function requestObjetivosPorRoteiro(idroteiro) {
+	var objetivos = getObjetivosPorRoteiro(idroteiro);
+	var htmlObjetivos = new String();
+
+	if (objetivos.length > 0) {
+		for (var a in objetivos) {
+			var classObj = new String();
+
+			if (objetivos[a].status === 1)
+				classObj = "objetivo_aberto";
+			else if (objetivos[a].status === 2)
+				classObj = "objetivo_entregue";
+
+			htmlObjetivos += "<div class=\"roteiro_item roteiro_objetivo\">";
+			htmlObjetivos += 	"Objetivo: "+objetivos[a].objetivo;
+			htmlObjetivos +=	"<div class=\"objetivo_status "+classObj+"\">&nbsp;</div>";
+			htmlObjetivos += "</div>";
+		}
+	} else {
+		htmlObjetivos += "<div calss=\"roteiro_item roteiro_objetivo\">Nenhum objetivo associado a este roteiro.</div>";
+	}
+
+	return htmlObjetivos;
+}
+function requestImagemPorPostagem(idpostagem) {
+	var imagem = getImagemPorPostagem(idpostagem);
+	var htmlImagem = new String();
+
+	if (imagem.length > 0)
+		htmlImagem += "<img src=\""+imagem[0]+"\" class=\"img_postagem\" />";
+
+	return htmlImagem;
+}
 
 function accessOficina(idoficina, corForte, corMedia, corFraca) {
 	$(".aba_oficina").removeClass("aba_ativa");
@@ -147,78 +216,26 @@ function accessOficina(idoficina, corForte, corMedia, corFraca) {
 	var postagens 	= requestBlogPostagensPorOficina(idoficina);
 	var roteiros 	= requestRoteirosPorOficina(idoficina,corForte,corMedia,corFraca);
 
-	$("#Postagens_Oficina").html(postagens);
+	$("#blogContainer").html(postagens);
 	$(".Acordeon_Oficina").html(roteiros);
 }
-function acordeon(index) {
-	var roteirosLista = $('.Oficina_Planejamento_Info');
-	var htmlObjContent = '';
-	var numObj = 0;
 
-	for ( var i = 0; i < roteirosLista.length; i++ ) {
-		if ( i == index ) {
-			if ( $($('.Oficina_Plan_Itens').get(i)).hasClass('expandido') ) {
-				$($('.Oficina_Plan_Itens').get(i)).slideUp();
-				$($('.Oficina_Plan_Itens').get(i)).removeClass('expandido');
-			} else {
-				if ( !$($('.Oficina_Plan_Itens').get(i)).hasClass('ObjsListados') ) {
-					for ( c in listaRotOficina) {
+function toggleRoteiro(idroteiro) {
+	$(".oficina_planejamento").not("#roteiro_"+idroteiro).removeClass("roteiro_expandido");
+	$(".oficina_planejamento").not("#roteiro_"+idroteiro).find(".roteiro_conteudo").slideUp();
 
-						// Seleciona o que foi clicado
-						if ( c == index ) {
+	if (!$("#roteiro_"+idroteiro).hasClass("objetivos_listados")) {
+		var objetivos = requestObjetivosPorRoteiro(idroteiro);
 
-							// Faz uma requisição para retornar os objetivos daquele roteiro
-							$.ajax({
-						    	url: path + 'ObjetivoAula/ListarPorRoteiro/' + listaRotOficina[c].idRoteiro_aula,
-						    	async: false,
-						    	type: "GET",
-						    	crossDomain: true,
-						   		beforeSend: function(){
-						   			loading("inicial");
-						   		},
-						    	success: function(dataObj) {
-						    		for ( a in dataObj ) {
-						    			var classObj;
+		$("#roteiro_"+idroteiro).addClass("objetivos_listados");
+		$("#roteiro_"+idroteiro).find(".roteiro_conteudo").append(objetivos);
+	}
 
-						    			// Verifica o status do objetivo
-							    		switch (dataObj[a].status) {
-											case 0:
-												classObj = '';
-											break;
-											
-											case 1: 
-												classObj = ' Aberto';
-											break;
-
-											case 2: 
-												classObj = ' Entregue';
-											break;
-										}
-
-										// Gera o código html para os objetivos
-										htmlObjContent +=
-										'<div class="Oficina_Plan_Item Oficina_Plan_Obj">Objetivo: '+
-											dataObj[a].objetivo+
-											'<div class="Oficina_Plan_Quadrado'+classObj+'">&nbsp;</div>'+
-										'</div>';
-										numObj++;
-						    		}
-						    	},
-								complete: function(){
-									loading("final");
-								}
-							});
-						}
-					}
-					$('#Oficina_Plan_Itens_'+(index+1)).append(htmlObjContent);
-					$($('.Oficina_Plan_Itens').get(i)).addClass('ObjsListados');
-				}
-				$($('.Oficina_Plan_Itens').get(i)).slideDown();
-				$($('.Oficina_Plan_Itens').get(i)).addClass('expandido');
-			}
-		} else {
-			$($('.Oficina_Plan_Itens').get(i)).slideUp();
-			$($('.Oficina_Plan_Itens').get(i)).removeClass('expandido');
-		}
+	if ($("#roteiro_"+idroteiro).hasClass("roteiro_expandido")) {
+		$("#roteiro_"+idroteiro).removeClass("roteiro_expandido");
+		$("#roteiro_"+idroteiro).find(".roteiro_conteudo").slideUp();
+	} else {
+		$("#roteiro_"+idroteiro).addClass("roteiro_expandido");
+		$("#roteiro_"+idroteiro).find(".roteiro_conteudo").slideDown();
 	}
 }
